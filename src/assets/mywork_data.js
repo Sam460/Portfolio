d424399d64998071611763236224523014673dae
Samrat <samratbiswas460@gmail.com>
2025-07-20 13:46:53 +0530

    commit

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -1,6 +1,6 @@
 import project1_img from '../assets/project_1.png'
 import project2_img from '../assets/project_2.png'
-// import project3_img from '../assets/project_3.svg'
+import project3_img from '../assets/project_3.png'
 // import project4_img from '../assets/project_4.svg'
 // import project5_img from '../assets/project_5.svg'
 // import project6_img from '../assets/project_6.svg'
@@ -11,21 +11,22 @@ const mywork_data = [
          w_name:"Upskill-Vision",
          w_img: project1_img,
          w_Git_link: "https://github.com/Sam460/Fullstack_UpskilVision_1.o",
-         w_Live_link: "https://fullstack-upskil-vision-1-o.vercel.app/login"
+         w_Live_link: "https://fullstack-upskil-vision-1-o.vercel.app/"
      },
-    // {
-    //     w_no:2,
-    //     w_name:"Tic-Tac-Toe",
-    //     w_img: project2_img,
-    //     w_Git_link: "https://github.com/tanmoy-paul01/Tic-Tac-Toe-Web-app",
-    //     w_Live_link: "https://tanmoy-paul01.github.io/Tic-Tac-Toe-Web-app/"
-    // },
-    // {
-    //     w_no:3,
-    //     w_name:"Web design",
-    //     w_img: project3_img,
-    //     // w_link:
-    // },
+     {
+         w_no:2,
+         w_name:"Fruits_Store",
+         w_img: project2_img,
+         w_Git_link: "https://github.com/Sam460/Fruits_Store",
+         w_Live_link: "https://fruits-store-iota.vercel.app/"
+     },
+     {
+         w_no:3,
+         w_name:"Web design",
+         w_img: project3_img,
+         w_Git_link:"https://github.com/Sam460/Calculator-project",
+         w_Live_link:"https://calculator-project-zeta-eight.vercel.app/"
+     },
     // {
     //     w_no:4,
     //     w_name:"Web design",
